249622c4af0d1ef366d7a70248054dbf1ab37182
Ganesh Punalkar <ganesh.punalkar@gmail.com>
2017-08-15 23:40:29 +0530

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }  from './components/app.components/app.component';
+import { UserComponent }  from './components/user.component/user.component';
+import { AboutComponent }  from './components/about.component/about.component';
+
+import { Routing }  from './config/app.routes';
+;
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, Routing, HttpModule, FormsModule ],
+  declarations: [ AppComponent, UserComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
